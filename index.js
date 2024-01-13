2433659249f0b1e2f99a2b9f5228437e1d7007c6
ayadag <109915071+ayadag@users.noreply.github.com>
2024-01-13 12:35:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const port = process.env.PORT || 5000;
 
 
 app.post("/api/v1/prediction/49a1fa06-e4df-4218-9911-008da1eb5620", (req, res) => {
-/*
+
   res.cookie("jwt", token,{
     expires: new Date(
       Date.now() + 2 * 24 * 60 * 60 * 1000
     ),
     secure: true,
     httpOnly: true,
-  });*/
+  });
   res.status(200).json({
     text:"Based on the given context, \"The Art of Goal Setting: Achieving Success through Personal Development\" is an essay or article that discusses the importance of goal setting in personal development and achieving success. It emphasizes the need for clear and compelling goals to ignite personal growth and outlines the benefits of visualization and self-awareness in the goal-setting process.",
   }
