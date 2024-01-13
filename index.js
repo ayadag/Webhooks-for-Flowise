fe66dbfb568c64f01846841885a27bd382009c70
ayadag <109915071+ayadag@users.noreply.github.com>
2024-01-13 12:07:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 
 app.post("/api/v1/prediction/49a1fa06-e4df-4218-9911-008da1eb5620", (req, res) => {
-
+/*
   res.cookie("jwt", token,{
     expires: new Date(
       Date.now() + 2 * 24 * 60 * 60 * 1000
@@ -14,7 +14,7 @@ app.post("/api/v1/prediction/49a1fa06-e4df-4218-9911-008da1eb5620", (req, res) =
     secure: true,
     httpOnly: true,
   });
-  
+  */
   res.status(200).json({
     status: "Success",
     token,
