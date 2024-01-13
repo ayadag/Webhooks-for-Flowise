10522e2fc198b11c3a0737ea8bb4dcbc619b1558
ayadag <109915071+ayadag@users.noreply.github.com>
2024-01-13 11:53:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const port = process.env.PORT || 5000;
 
 app.post("/api/v1/prediction/49a1fa06-e4df-4218-9911-008da1eb5620", (req, res) => {
 
-  res.json({"text":"Based on the given context, \"The Art of Goal Setting: Achieving Success through Personal Development\" is an essay or article that discusses the importance of goal setting in personal development and achieving success. It emphasizes the need for clear and compelling goals to ignite personal growth and outlines the benefits of visualization and self-awareness in the goal-setting process."})
-  res.status(200);
-  res.end();
-})
+  res.cookie("_cfuvid", token,{
+    expires: Session,
+    secure: true,
+    httpOnly: true,
+  });
+  
+  res.status(200).json({
+    status: "Success",
+    token,
+    data: {"text":"Based on the given context, \"The Art of Goal Setting: Achieving Success through Personal Development\" is an essay or article that discusses the importance of goal setting in personal development and achieving success. It emphasizes the need for clear and compelling goals to ignite personal growth and outlines the benefits of visualization and self-awareness in the goal-setting process."},
+    
+  });
+  
+});
 app.listen(port, () => console.log("server running"));
